a5758f9021a24771cfa0a6c21f3c64f22ec6eb90
iba <iba@e-mail.ru>
2023-06-12 23:43:34 +0300

    minor changes

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set  -e
-docker-compose down --rmi local
+docker compose down --rmi local
 git pull
-docker-compose up
-docker-compose exec backend ./manage.py migrate --noinput
+docker compose up
+docker compose exec backend ./manage.py migrate --noinput
 echo "Deploy successful!!!"
 
